ea27307d5eae7bb231f30df273508814b294f2a8
Lucas Spitzer <lucas.spitzer@outlook.fr>
2024-06-24 00:37:09 +0200

    removing navbar useless buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,10 +7,6 @@
         <v-app-bar-title>View Todo</v-app-bar-title>
 
         <template v-slot:append>
-            <v-btn icon="mdi-plus"></v-btn>
-
-            <v-btn icon="mdi-magnify"></v-btn>
-
             <v-btn icon="mdi-dots-vertical"></v-btn>
         </template>
     </v-app-bar>
