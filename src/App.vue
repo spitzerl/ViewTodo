e64d02c5d76050aefb3d265cb52c8d5f854859a4
Lucas Spitzer <lucas.spitzer@outlook.fr>
2024-06-23 14:07:50 +0200

    separating navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar :elevation="14">
-        <!-- <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template> -->
-
-        <v-app-bar-title>View Todo</v-app-bar-title>
-
-        <template v-slot:append>
-          <v-btn icon="mdi-plus"></v-btn>
-
-          <v-btn icon="mdi-magnify"></v-btn>
-
-          <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
-      </v-app-bar>
+      <Navbar />
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-//
-</script>
+<script setup></script>
