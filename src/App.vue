2fcab33a632e4d04a638942156616c1cf7572679
Lucas Spitzer <lucas.spitzer@outlook.fr>
2024-06-24 23:40:31 +0200

    adding button to page body

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-app>
-    <v-main>
-      <Navbar />
-      <ToDo />
-    </v-main>
-  </v-app>
+    <v-app :theme="theme">
+        <v-main>
+            <Navbar />
+            <ToDo />
+        </v-main>
+        <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" text="Toggle Theme" slim @click="onClick"></v-btn>
+    </v-app>
 </template>
 
-<script setup></script>
+<script setup>
+    import { ref } from 'vue'
+
+    const theme = ref('light')
+
+    function onClick() {
+        theme.value = theme.value === 'light' ? 'dark' : 'light'
+    }
+</script>
 
 <style></style>
