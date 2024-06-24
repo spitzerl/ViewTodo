b07d1d1d71f414a06b6b4b7769f60e77795bdc1a
Lucas Spitzer <lucas.spitzer@outlook.fr>
2024-06-24 09:46:13 +0200

    add button theme change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app-bar :elevation="14">
+    <v-app-bar :elevation="14" class="px-2">
         <template v-slot:prepend>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template>
@@ -7,7 +7,7 @@
         <v-app-bar-title>View Todo</v-app-bar-title>
 
         <template v-slot:append>
-            <v-btn icon="mdi-dots-vertical"></v-btn>
+            <v-btn :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="toggleTheme"></v-btn>
         </template>
     </v-app-bar>
 </template>
